67a666cb1e996a2ea3c0944834257324ad6b3c01
dnadjar <115176672+dnadjar@users.noreply.github.com>
2026-03-25 12:20:48 +0100

    Question 4

--- student_records.py
+++ student_records.py
@@ -7,7 +7,7 @@ class Student:
         return sum(self.grades) / len(self.grades)
 
     def __str__(self):
-        return f"{self.name} (M1: {self.grades[0]}, Moyenne: {self.get_average():.2f})"
+        return f"{self.name} (M1: {self.grades[0]}, M2: {self.grades[1]}, M3: {self.grades[2]}, Moyenne: {self.get_average():.2f})"
 
 class SchoolClass:
     def __init__(self):
@@ -17,9 +17,20 @@ class SchoolClass:
         self.students.append(student)
 
     def rank_matter_1(self):
-        # Tri décroissant sur l'index 0 de la liste grades
         sorted_students = sorted(self.students, key=lambda s: s.grades[0], reverse=True)
-        print("Classement Matière 1 :")
+        print("\nClassement Matière 1 :")
+        for student in sorted_students:
+            print(student)
+
+    def rank_matter_2(self):
+        sorted_students = sorted(self.students, key=lambda s: s.grades[1], reverse=True)
+        print("\nClassement Matière 2 :")
+        for student in sorted_students:
+            print(student)
+
+    def rank_matter_3(self):
+        sorted_students = sorted(self.students, key=lambda s: s.grades[2], reverse=True)
+        print("\nClassement Matière 3 :")
         for student in sorted_students:
             print(student)
 
@@ -30,3 +41,5 @@ if __name__ == '__main__':
     school_class.add_student(Student('V', 9, 14, 14))
 
     school_class.rank_matter_1()
+    school_class.rank_matter_2()
+    school_class.rank_matter_3()
