9c6ab9bfdd7b5549741c16405b80ee2d59a1f52b
dnadjar <115176672+dnadjar@users.noreply.github.com>
2026-03-25 12:19:35 +0100

    Question 3

--- student_records.py
+++ student_records.py
@@ -7,7 +7,7 @@ class Student:
         return sum(self.grades) / len(self.grades)
 
     def __str__(self):
-        return f"{self.name} (Moyenne: {self.get_average():.2f})"
+        return f"{self.name} (M1: {self.grades[0]}, Moyenne: {self.get_average():.2f})"
 
 class SchoolClass:
     def __init__(self):
@@ -16,7 +16,17 @@ class SchoolClass:
     def add_student(self, student: Student):
         self.students.append(student)
 
-school_class = SchoolClass()
-school_class.add_student(Student('J', 10, 12, 13))
-school_class.add_student(Student('A', 8, 2, 17))
-school_class.add_student(Student('V', 9, 14, 14))
+    def rank_matter_1(self):
+        # Tri décroissant sur l'index 0 de la liste grades
+        sorted_students = sorted(self.students, key=lambda s: s.grades[0], reverse=True)
+        print("Classement Matière 1 :")
+        for student in sorted_students:
+            print(student)
+
+if __name__ == '__main__':
+    school_class = SchoolClass()
+    school_class.add_student(Student('J', 10, 12, 13))
+    school_class.add_student(Student('A', 8, 2, 17))
+    school_class.add_student(Student('V', 9, 14, 14))
+
+    school_class.rank_matter_1()
